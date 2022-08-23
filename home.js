16f8dbc8652e0e470e9f70587df04c466e4ec12c
Farrah Jean <farrahjean99@gmail.com>
2022-08-23 15:27:45 -0400

    Committed onto Master

---
+++ home.js
@@ -0,0 +1,11 @@
+function Home(){
+  return (
+    <Card
+      txtcolor="blue"
+      header="Worst Bank App"
+      title="Welcome only if you dare..."
+      text="Use the navigation bar and have fun!"
+      body={(<img src="badbankimage.png" className="img-fluid" alt="Responsive image"/>)}
+    />    
+  );  
+}
